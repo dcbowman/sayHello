856aa3b010d447de84b1f68cef4457c787f1d70f
Danielle Bowman <dcbowman6@yahoo.com>
2016-09-04 13:12:41 -0400

    delete mySQL and connected mongodb

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 // Include Server Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+//Database configuration
+var mongojs = require('mongojs');
+var databaseUrl = "guests";
+var collections = ["guests"];
 
 // Create Instance of Express
 var app = express();
 var PORT = process.env.PORT || 3000; // Sets an initial port. 
 
+//mongo js to hook the database to the db variable
+var db = mongojs(databaseUrl, collections);
 
+//logs any mongodb errors
+db.on('error', function(err){
+	console.log('Database Error:', err);
+})
 
 // Listener
 app.listen(PORT, function() {
